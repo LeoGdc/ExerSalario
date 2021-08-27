0207d0d13155dd984e70f789931881db08f4c549
LEONARDO DE NOVAES VIVI <21276367@CFP127ALUNO.LOCAL>
2021-08-27 16:56:54 -0300

    resovendo bugs

--- src/Salario.java
+++ src/Salario.java
@@ -11,24 +11,24 @@ public class Salario {
 		System.out.println("Qual seu o nome do vendedor?");
 		name = leitor.next();
 
-		System.out.println(
-				"Quantos o vendedor tem de contribuição?");
+		System.out.println("Quantos o vendedor tem de contribuição?");
 		ano = leitor.nextDouble();
 
 		System.out.println("Qual o valor total de vendas ele fez?");
 		V = leitor.nextDouble();
 
-		System.out.println("Parabens " + name);
+		System.out.println("Parabéns " + name);
 
 		if (ano >= 1) {
 			if (V >= 20000) {
 				System.out.println("sua comissão é de " + V * 10 / 100);
-			} else {
+			} else if (V >= 10000) {
 				System.out.println("sua comissão é de " + V * 5 / 100);
+			} else {
+				System.out.println("sua comissão é de " + V * 3 / 100);
 			}
-
 		} else {
-			System.out.println("sua comissão é " + V * 3 / 100);
+			System.out.println("sua comissão é de " + V * 3 / 100);
 		}
 
 		leitor.close();
